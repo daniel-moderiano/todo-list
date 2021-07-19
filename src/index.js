ad27d7a5d574456f94bd1a967360b071dd3aeceb
Daniel <daniel.moderiano@outlook.com>
2021-07-19 20:39:53 +0930

    Added functionality to only enable add btn when title input contains text

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import { createTodoForm } from "./scripts/view";
-import { addModalControls, btnEnable } from "./scripts/controller";
+import { addModalControls, todoFormBtnContol } from "./scripts/controller";
 import { todos } from "./scripts/model";
 
 // "main" style funciton to run appropriate functions from the page load
 function main() {
   addModalControls();
-}
-
-while (document.querySelector(".add-modal").style.display == "block") {
-  const todoFormBtn = document.querySelector(".todo-form__btn");
-  if (document.querySelector("#todo-form__title").value != "") {
-    todoFormBtn.disabled = false;
-  } else {
-    todoFormBtn.disabled = true;
-  }
+  todoFormBtnContol();
 }
 
 main();
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -32,6 +32,9 @@ function addModalControls() {
   newTodo.addEventListener("click", clearInputs);
   newTodo.addEventListener("click", displayModal);
 
+  // Get "add task" btn
+  const todoFormBtn = document.querySelector(".todo-form__btn");
+
   // Listen for outside click
   window.addEventListener("click", outsideClick);
   
@@ -52,6 +55,7 @@ function addModalControls() {
     document.querySelector("#todo-form__description").value = "";
     document.querySelector("#todo-form__date").value = "";
     document.querySelector("#todo-form__priority").value = "";
+    todoFormBtn.disabled = true;
   }
 
   // Close the modal on outside click
@@ -61,23 +65,40 @@ function addModalControls() {
     }
   }
 
-  // Get "add task" btn
-  const todoFormBtn = document.querySelector(".todo-form__btn");
-
   // Listen for form submit/"add todo" btn click
   todoFormBtn.addEventListener("click", addNewTodo);
   todoFormBtn.addEventListener("click", closeModal);
 }
 
+function todoFormBtnContol(params) {
+  // Get form input
+  const todoFormTitle = document.querySelector("#todo-form__title");
+
+  // Get form add task btn
+  const todoFormBtn = document.querySelector(".todo-form__btn");
+
+  // Det default btn state to disabled
+  todoFormBtn.disabled = true;
+
+  // Listen for user typing or entering data into input
+  todoFormTitle.addEventListener("input", btnEnable);
+  // todoFormTitle.addEventListener("paste", () => {
+  //   console.log("pasted");
+  //   console.log(document.querySelector("#todo-form__title").value);
+  // });
+
+  btnEnable();
+}
+
 // Enable add btn only if user enters a title for the todo
 function btnEnable() {
   const todoFormBtn = document.querySelector(".todo-form__btn");
-  if (document.querySelector("#todo-form__title").value != "") {
-    todoFormBtn.disabled = false;
-  } else {
+  if (document.querySelector("#todo-form__title").value.trim() === "") {
     todoFormBtn.disabled = true;
+  } else {
+    todoFormBtn.disabled = false;
   }
 }
 
 
-export { addModalControls, btnEnable }
+export { addModalControls, todoFormBtnContol }
