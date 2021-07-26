53206f984656096b39d19df061c2ef63ef552dfa
Daniel <daniel.moderiano@outlook.com>
2021-07-26 11:41:31 +0930

    Moved new task button under todo list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { refreshTodoList, removeListElement, renderAllTodos, renderList, setSelectedClass } from "./scripts/view";
 import { addAllListControls, addModalControls, addSidebarControls, todoFormBtnContol, listModalBtnControls } from "./scripts/controller";
 import { todos, getFromStorage, addToStorage, lists, selectedList, removeList, changeList } from "./scripts/model";
-import { format } from 'date-fns';
 
 // "main" style funciton to run appropriate functions on initial page load
 function main() {
@@ -12,19 +11,9 @@ function main() {
   addSidebarControls();
   todoFormBtnContol();
   listModalBtnControls();
-  
-  let date = '2021-07-27';
-  console.log(dateFormatter(date));
-
 }
 
 main();
 
-// Take output from HTML date picker, and format to a better representation for UI
-function dateFormatter(dateStr) {
-  let year = parseInt(dateStr.slice(0, 4));
-  let month = parseInt(dateStr.slice(5, 7)) - 1;
-  let day = parseInt(dateStr.slice(8, 10));
-  return format(new Date(year, month, day), 'd MMM yyyy');
-}
+
 
