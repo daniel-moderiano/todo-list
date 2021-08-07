58f6f6077d6e26f56eee0e0608fa094dbd267509
Daniel <daniel.moderiano@outlook.com>
2021-08-07 18:19:39 +0930

    Trialing Babel loader with Webpack -- success

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
 };
