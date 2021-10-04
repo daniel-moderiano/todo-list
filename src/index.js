f38c5006c0213313b1c51403e556c9003e8852ce
Daniel <daniel.moderiano@outlook.com>
2021-10-04 12:16:47 +1030

    Added babel-related imports to allow async/await to run at top level

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import { doc, getDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import { refreshTodoList, addSidebarCollapseControls, refreshSidebarLists } from './scripts/view';
 import { addModalControls, addSidebarControls, listModalBtnControls, addNewTaskControls, todoEventListeners, sidebarEventListeners, viewModalBtnControls } from './scripts/controller';
 import { addToStorage, checkStorage } from './scripts/model';
+import database from './scripts/firebase';
 
 // "main" style funciton to run appropriate functions on initial page load
 function main() {
@@ -17,5 +21,20 @@ function main() {
   sidebarEventListeners();
   addSidebarCollapseControls();
 }
+
+// Grab data once from firestore db and log to console
+(async () => {
+  const testCollection = collection(database, 'test');
+  const q = query(testCollection, where('name', '==', 'sam'));
+
+  const querySnapshot = await getDocs(q);
+  console.log(querySnapshot);
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.date());
+  });
+})().catch(e => {
+  // Error handling
+});
 
 main();
